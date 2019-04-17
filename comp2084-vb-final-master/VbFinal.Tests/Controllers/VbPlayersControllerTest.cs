f40d1585c024e25e55cb98adec8cfe58b14b0862
ManuCherian <46466036+ManuCherian@users.noreply.github.com>
2019-04-17 12:27:41 -0400

    first commit

---
+++ comp2084-vb-final-master/VbFinal.Tests/Controllers/VbPlayersControllerTest.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using VbFinal.Controllers;
+using VbFinal.Models;
+
+namespace VbFinal.Tests.Controllers
+{
+    [TestClass]
+    public class VbPlayersControllerTest
+    {
+        VbPlayersController controller;
+        Mock<IMockVbPlayer> mock;
+        List<VbPlayer> vbPlayers;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            mock = new Mock<IMockVbPlayer>();
+
+            vbPlayers = new List<VbPlayer>
+            {
+                new VbPlayer
+                {
+                    VbPlayerId = 961,
+                    FirstName = "Lori",
+                    Lastname = "S",
+                    Photo = "https://img.icons8.com/color/384/beach-volleyball.png"
+                },
+                new VbPlayer
+                {
+                    VbPlayerId = 961,
+                    FirstName = "Zak",
+                    Lastname = "N",
+                    Photo = "https://img.icons8.com/ultraviolet/384/beach-volleyball.png"
+                }
+            };
+
+            mock.Setup(m => m.VbPlayers).Returns(vbPlayers.AsQueryable());
+            controller = new VbPlayersController(mock.Object);
+        }
+
+        [TestMethod]
+        public void IndexViewLoads()
+        {
+            // arrange
+            // now handled in TestInitialize
+
+            // act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // assert
+            Assert.AreEqual("Index", result.ViewName);
+        }
+
+        public void IndexValidLoadsVbPlayers()
+        {
+            //act 
+
+
+            //assert
+
+
+        }
+
+        public void EditLoadsValidId()
+        {
+            //act 
+
+
+            //assert
+
+
+        }
+        public void EditLoadsVbPlayerValidId()
+        {
+            //act 
+
+
+            //assert
+
+
+        }
+
+        public void EditInvalidId()
+        {
+            //act 
+
+
+            //assert
+
+
+        }
+
+        public void EditNoId()
+        {
+            //act 
+
+
+            //assert
+
+
+        }
+
+
+        public void EditSaveInvalid()
+        {
+            //act 
+
+
+            //assert
+
+
+        }
+
+        public void EditSaveValid()
+        {
+            //act 
+
+
+            //assert
+
+
+
+        }
+
+
+
+
+    }
+}
